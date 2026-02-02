2b38714ef51737f722487f9faf2969251eea8388
Youcef <youcefcopy@gmail.com>
2026-02-02 11:35:24 +0100

    toggling two different layout when clicking get started

--- src/main.js
+++ src/main.js
@@ -1,2 +1,5 @@
 // this is the main js file
 console.log('js is working');
+import { loadPage } from "./ui/event.js";
+
+loadPage();
--- src/ui/event.js
+++ src/ui/event.js
@@ -1,1 +1,13 @@
 // event listeners (click, submit)
+const getStartedBtn = document.querySelector('#getStartedBtn');
+
+function loadPage() {
+    getStartedBtn.addEventListener('click', () => {
+        console.log('getting started');
+        console.log(getStartedBtn.closest('.hero-section'));
+        getStartedBtn.closest('.hero-section').classList.add('hidden');
+        document.querySelector('.main-section').classList.remove('hidden');
+    })
+}
+
+export {loadPage };
