620c5d7383c7acccec676955331445aa13586b77
Youcef <youcefcopy@gmail.com>
2026-02-02 12:41:46 +0100

    something's wrong with styling the container

--- src/ui/render.js
+++ src/ui/render.js
@@ -5,8 +5,12 @@ function renderShows(container) {
     shows.forEach(
         show => {
             const card = document.createElement('div');
+            const cardImg = document.createElement('img');
             card.classList.add('card');
+            cardImg.src = show.poster;
+            cardImg.alt = show.title;
 
+            card.appendChild(cardImg);
             container.appendChild(card);
         }
     )
