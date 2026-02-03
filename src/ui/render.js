8a1dccc4f4f5e7c51dd4b8fce21561b87134c0e2
Youcef <youcefcopy@gmail.com>
2026-02-03 09:23:23 +0100

    it's alright to use innerHTML in this case

--- src/ui/render.js
+++ src/ui/render.js
@@ -5,12 +5,15 @@ function renderShows(container) {
     shows.forEach(
         show => {
             const card = document.createElement('div');
-            const cardImg = document.createElement('img');
+            // const cardImg = document.createElement('img');
             card.classList.add('card');
-            cardImg.src = show.poster;
-            cardImg.alt = show.title;
+            // cardImg.src = show.poster;
+            // cardImg.alt = show.title;
 
-            card.appendChild(cardImg);
+            card.innerHTML = `
+                <img src = "${show.poster}" alt = "${show.title}">
+            `;
+            // card.appendChild(cardImg);
             container.appendChild(card);
         }
     )
