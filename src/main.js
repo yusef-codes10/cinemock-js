8cc967bbacccf947d2fab97e76b12dbd65419c9e
Youcef <youcefcopy@gmail.com>
2026-02-03 14:30:22 +0100

    event delegation on the navbar

--- src/main.js
+++ src/main.js
@@ -1,12 +1,14 @@
 // this is the main js file
 console.log('js is working');
-import { getStarted, addToFavorite, isFavoriteList, setFavoritList } from "./ui/event.js";
+import { getStarted, addToFavorite, setFavoritList } from "./ui/event.js";
 import { renderShows } from "./ui/render.js";
 import { shows } from "./logic/shows.js";
+import { clickNav } from "./ui/nav.js";
 
 const mainSection = document.querySelector('.main-section');
 const cardsContainer = document.querySelector('.cards');
 const signUpBtn = document.getElementById('signUpBtn');
+const navList = document.querySelector('.nav-list ul');
 
 getStarted();
 
@@ -28,3 +30,7 @@ signUpBtn.addEventListener('click',
         renderShows(cardsContainer);
     }
 )
+
+console.log(navList);
+
+clickNav(navList);
---
+++ src/ui/nav.js
@@ -0,0 +1,23 @@
+// navigatin logic
+
+
+function clickNav(nav) {
+    nav.addEventListener('click', e => {
+        const link = e.target.closest('a');
+        if (!link) return;
+        e.preventDefault(); // Optional: stops default link behavior
+
+        if (link.textContent === 'Home') {
+            console.log('hhome');
+        } if (link.textContent === 'TV Series') {
+            console.log('TV Series');
+        } if (link.textContent === 'Movies') {
+            console.log('movis');
+        } if (link.textContent === 'Settings') {
+            console.log('Settings');
+        }
+    })
+
+}
+
+export {clickNav};
