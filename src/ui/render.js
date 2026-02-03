4c21b1ff680d9a55e54e099fa539c9a8a5e930a6
Youcef <youcefcopy@gmail.com>
2026-02-03 11:05:46 +0100

    we should use the same function to render favorite shows too

--- src/ui/render.js
+++ src/ui/render.js
@@ -22,4 +22,25 @@ function renderShows(container) {
     )
 };
 
+// function renderFavoriteShows(container) {
+//         shows.forEach(
+//         show => {
+//             const card = document.createElement('div');
+//             // const cardImg = document.createElement('img');
+//             card.classList.add('card');
+//             // cardImg.src = show.poster;
+//             // cardImg.alt = show.title;
+
+//             card.dataset.id = show.id; // 🔑 LINK UI → DATA
+//             card.innerHTML = `
+//                 <img src = "${show.poster}" alt = "${show.title}">
+//                 <i class="fa-solid fa-star star"></i>
+
+//             `;
+//             // card.appendChild(cardImg);
+//             container.appendChild(card);
+//         }
+//     )
+// }
+
 export {renderShows};
