32baa9340b9103c4ada072bba5fd3f4dde7f3dfc
Youcef <youcefcopy@gmail.com>
2026-02-04 11:45:19 +0100

    preparing for laod from localStorage

--- src/ui/render.js
+++ src/ui/render.js
@@ -5,6 +5,8 @@ import { getFavoriteShows, isFavoriteList } from "../ui/event.js";
 function renderShows(container) {
     const list = isFavoriteList() ? getFavoriteShows(shows) : shows;
 
+    // !2- load from local Storage
+
     container.innerHTML = ''; // clear before re-rendering
     list.forEach(
         show => {
@@ -29,26 +31,5 @@ function renderShows(container) {
         }
     )
 };
-
-// function renderFavoriteShows(container) {
-//         shows.forEach(
-//         show => {
-//             const card = document.createElement('div');
-//             // const cardImg = document.createElement('img');
-//             card.classList.add('card');
-//             // cardImg.src = show.poster;
-//             // cardImg.alt = show.title;
-
-//             card.dataset.id = show.id; // 🔑 LINK UI → DATA
-//             card.innerHTML = `
-//                 <img src = "${show.poster}" alt = "${show.title}">
-//                 <i class="fa-solid fa-star star"></i>
-
-//             `;
-//             // card.appendChild(cardImg);
-//             container.appendChild(card);
-//         }
-//     )
-// }
 
 export {renderShows};
